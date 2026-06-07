3a0218a2bb29103176587299665c935d29167413
Enzo F. Borges <enzo.alternativo@hotmail.com>
2026-06-07 18:54:25 -0300

    feat: adiciona persistência da lista de tarefas #7

--- src/task.py
+++ src/task.py
@@ -2,7 +2,7 @@ import time
 import uuid
 
 class Task:
-    def __init__(self, description: str, priority: int, task_id: str = None):
+    def __init__(self, description: str, priority: int, task_id: str = None, created_at: float = None):
         """
         Representa uma tarefa individual no sistema.
         - description: Breve descrição da tarefa.
@@ -12,7 +12,24 @@ class Task:
         self.id = task_id if task_id else uuid.uuid4().hex[:8] # id até 8 caract
         self.description = description
         self.priority = priority
-        self.created_at = time.time() # tempo para ordenação secundária, se necessárria
+        self.created_at = created_at if created_at else time.time() # tempo para ordenação secundária, se necessárria
+
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "description": self.description,
+            "priority": self.priority,
+            "created_at": self.created_at
+        }
+
+    @classmethod
+    def from_dict(cls, data: dict) -> 'Task':
+        return cls(
+            description=data["description"],
+            priority=data["priority"],
+            task_id=data["id"],
+            created_at=data["created_at"]
+        )
 
     def __repr__(self):
         return f"Task(id='{self.id}', description='{self.description}', priority={self.priority})"
