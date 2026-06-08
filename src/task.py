ce8ebe6e0ca6360e9d4a45acb451d2108ba1d9e0
rayenealmeida <rayene.almeida@ibm.com>
2026-06-08 10:17:01 -0300

    feat: CRUD das tasks

--- src/task.py
+++ src/task.py
@@ -2,24 +2,27 @@ import time
 import uuid
 
 class Task:
-    def __init__(self, description: str, priority: int, task_id: str = None, created_at: float = None):
+    def __init__(self, description: str, priority: int, task_id: str = None, created_at: float = None, completed: bool = False):
         """
         Representa uma tarefa individual no sistema.
         - description: Breve descrição da tarefa.
         - priority: Valor inteiro de 1 a 100 indicando a urgência da tarefa.
         - task_id: Identificador único opcional (caso não fornecido, gera um UUID).
+        - completed: Status de conclusão da tarefa.
         """
         self.id = task_id if task_id else uuid.uuid4().hex[:8] # id até 8 caract
         self.description = description
         self.priority = priority
         self.created_at = created_at if created_at else time.time() # tempo para ordenação secundária, se necessárria
+        self.completed = completed
 
     def to_dict(self) -> dict:
         return {
             "id": self.id,
             "description": self.description,
             "priority": self.priority,
-            "created_at": self.created_at
+            "created_at": self.created_at,
+            "completed": self.completed
         }
 
     @classmethod
@@ -28,8 +31,9 @@ class Task:
             description=data["description"],
             priority=data["priority"],
             task_id=data["id"],
-            created_at=data["created_at"]
+            created_at=data["created_at"],
+            completed=data.get("completed", False)
         )
 
     def __repr__(self):
-        return f"Task(id='{self.id}', description='{self.description}', priority={self.priority})"
+        return f"Task(id='{self.id}', description='{self.description}', priority={self.priority}, completed={self.completed})"
